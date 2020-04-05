b046fb222dea039013dfbe915d1c04481f4bdf7d
Saida Davis <saihdavis@gmail.com>
2020-04-05 18:07:07 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -58,7 +58,7 @@ def monopoly_with_fourth_tier
         :shortline => {"mortgage_value" => "$800"}
       },
       :rent_in_dollars => {
-        :one_piece_owned => 500,
+        :one_piece_owned => 25,
         :two_pieces_owned => 50,
         :three_pieces_owned => 100,
         :four_pieces_owned => 400}
