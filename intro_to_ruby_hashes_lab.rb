097be4c407464c5bef5f6dbc83749982ae46c8b2
Saida Davis <saihdavis@gmail.com>
2020-04-05 18:06:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -59,7 +59,7 @@ def monopoly_with_fourth_tier
       },
       :rent_in_dollars => {
         :one_piece_owned => 500,
-        :two_pieces_owned => 600,
+        :two_pieces_owned => 50,
         :three_pieces_owned => 100,
         :four_pieces_owned => 400}
       }
